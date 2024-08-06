e151105715cda71b99ba2b81619e35deabf239f1
sobia-r <sobia.rasheed@gmail.com>
2024-08-06 23:06:11 +0400

    Navbar Modifications according to theme

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -2,16 +2,13 @@ import styles from "./NavBarStyles.module.css"
 import logo from "../../assets/fresh-burger.png"
 function NavBar() {
   return (
-    <nav className={styles.navbar}>
-        <div className={styles.leftlogo}>
-            <img className={styles.logoImage}  src={logo} alt="Web logo"/>
-        </div>
-        
+    <nav className={styles.container}>
+        <img className={styles.logoImage}  src={logo} alt="Web logo"/>
         <ul className={styles.navmenu}>
-            <li><a href="#hero">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#hero"><h2>About</h2></a></li>
+            <li><a href="#projects"><h2>Projects</h2></a></li>
+            <li><a href="#skills"><h2>Skills</h2></a></li>
+            <li><a href="#contact"><h2>Contact</h2></a></li>
         </ul>
     </nav>
   )
